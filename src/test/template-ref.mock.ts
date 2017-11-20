ac4758396cf8e978c0f63d167901ce5c077cfd84
Martin Sonnenholzer <chaosmonster.github.io@googlemail.com>
2017-11-20 21:14:25 +0100

    Update to Angular version5

--- src/test/template-ref.mock.ts
+++
@@ -1,9 +0,0 @@
-import {ElementRef, EmbeddedViewRef, TemplateRef} from '@angular/core';
-
-export class TemplateRefMock<C> extends TemplateRef<C> {
-  elementRef: ElementRef = null;
-
-  createEmbeddedView(context: C): EmbeddedViewRef<C> {
-    return undefined;
-  }
-}
